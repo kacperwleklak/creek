00a82dec3034463d135b838b254799cb24fc3f28
Kacper Wleklak <kacper.wleklak@student.put.poznan.pl>
2023-04-25 23:03:15 +0200

    rubis dbscripts

--- script/runner_2/replica1.bat
+++
@@ -1,9 +0,0 @@
-set PG_PORT=5433
-set DBNAME=./creek1
-set COMMUNICATION_REPLICAS_PORT=10001
-set COMMUNICATION_REPLICAS_HOST=localhost
-set SERVER_PORT=8081
-set COMMUNICATION_REPLICAS_NODES=localhost:10001,localhost:10002
-set COMMUNICATION_REPLICAS_ID=1
-
-java -jar -Xms512m -Xmx1024m ..\..\creek-impl\target\creek-impl-0.0.1-SNAPSHOT.jar
